48f9fd9f10c91fa1ca870be6fc536ea25c902973
wisestMe <wyhzest@gmail.com>
2019-07-27 03:32:18 +0100

    modifies src ref for drag and drop js file

--- js/dragDrop.js
+++ js/dragDrop.js
@@ -2,3 +2,5 @@ function addDnDHandlers() {
 	let productImages = document.querySelectorAll('.product-article');
 	let shoppingCartDropZone =document.querySelector('#shopping-cart');
 }
+
+console.log(productImages);
