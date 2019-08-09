a10f6b270c773d5d47529c0a4151434b021432a1
wisestMe <wyhzest@gmail.com>
2019-08-09 03:31:58 +0100

    tests rest parameters

--- js/app.js
+++ js/app.js
@@ -3,3 +3,29 @@ var cartAdd  = document.getElementById('add-to-cart');
 cartAdd.onclick = function () {
 	alert('item added to your cart');
 }
+
+function sendCars(...carIDs) {
+	carIDs.forEach(id => console.log(id));
+}
+//sendCars(4, 10, 6);
+//console.log(sendCars(4, 10, 6));
+
+
+function restP(s, ...arr) {
+	
+	for(i=0; i < 6; i++) {
+		s = 0;
+		s += i;
+		console.log(s);
+
+	}
+	console.log(s);
+	return s;
+}
+//console.log(s);
+restP(1, 2, 3, 4, 5, 6, 7, 8);
+
+const checkArr = [5, 7, 9, 8];
+
+const newArr  = [67, ...checkArr];
+console.log(newArr);
