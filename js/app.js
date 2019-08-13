f182ddb6d76011297eae29a840e06c172bcf8ea8
wisestMe <wyhzest@gmail.com>
2019-08-13 21:55:55 +0100

    passes arguement to the function from the call method

--- js/app.js
+++ js/app.js
@@ -4,7 +4,21 @@ cartAdd.onclick = function () {
 	alert('item added to your cart');
 };
 
+let phone = {
+	myPhone: function(nationality) {
+		return 'I use a ' + nationality + ' made ' + this.name + ' ' + this.model + ' phone';
+	},
+}
 
+let GalaxyNoteTen = {
+	name: 'Samsung',
+	model: 'Galaxy Note 10',
+}
 
+let nokiaActive = {
+	name: 'Nokia',
+	model: 'Active A8',
+}
 
-
+let caller = phone.myPhone;
+console.log(caller.call(nokiaActive, 'Nigerian'));
