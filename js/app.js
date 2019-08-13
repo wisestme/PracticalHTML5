d1f2ec599627b21784691fd619cb575aaea2e738
wisestMe <wyhzest@gmail.com>
2019-08-13 07:05:54 +0100

    deletes test codes

--- js/app.js
+++ js/app.js
@@ -4,20 +4,5 @@ cartAdd.onclick = function () {
 	alert('item added to your cart');
 };
 
-let app = (function() {
-	let carId = 123;
-	let getId = function() {
-		return carId;
-	};
-	return {
-		getId: getId,
-	};
-})();
-console.log(app.getId());
 
-let theReturn = function() {
-	let vari = 'an string';
-	return vari;
-}
 
-console.log(theReturn());
