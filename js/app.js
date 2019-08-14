5be241884b2d3eacbf1de818f2ae66b59468e04d
wisestMe <wyhzest@gmail.com>
2019-08-14 14:51:19 +0100

    implements default parameters

--- js/app.js
+++ js/app.js
@@ -4,3 +4,9 @@ cartAdd.onclick = function () {
 	alert('item added to your cart');
 };
 
+let trackCar = (carId, city='Aba') => {
+	output = `Tracking ${carId} in ${city}`;
+	console.log(output);
+}
+
+trackCar('Rols Royce');
