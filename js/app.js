e59e27b1e5992321cc84e384e7fe081fbae942e1
wisestMe <wyhzest@gmail.com>
2019-08-14 14:45:56 +0100

    deletes test codes

--- js/app.js
+++ js/app.js
@@ -4,39 +4,3 @@ cartAdd.onclick = function () {
 	alert('item added to your cart');
 };
 
-let phone = {
-	myPhone: function(color, nationality) {
-		return 'I use a ' + color + ' ' + nationality + ' made ' + this.name + ' ' + this.model + ' phone';
-	},
-}
-
-let GalaxyNoteTen = {
-	name: 'Samsung',
-	model: 'Galaxy Note 10',
-}
-
-let nokiaActive = {
-	name: 'Nokia',
-	model: 'Active A8',
-}
-
-let caller = phone.myPhone;
-console.log(caller.apply(nokiaActive, ['white', 'Nigerian']));
-
-var module = {
-  x: 42,
-  getX: function() {
-    return this.x;
-  }
-}
-
-var unboundGetX = module.getX;
-console.log(unboundGetX.bind(module)());
-
-let arrow = (luckyNumber) => {
-	let outt = luckyNumber + 123;
-	console.log(typeof(outt));
-	 return outt;
-}
-
-console.log(arrow('Lucky Number: '));
