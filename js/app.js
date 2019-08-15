285c7216733d3497b4fc48db1c8372d28a81b6dd
wisestMe <wyhzest@gmail.com>
2019-08-15 18:53:11 +0100

    implements a constructor function

--- js/app.js
+++ js/app.js
@@ -4,3 +4,13 @@ cartAdd.onclick = function () {
 	alert('item added to your cart');
 };
 
+function Car (name) {
+this.carId = name;
+this.start  = function() {
+	console.log('Start: ' + this.carId);
+};
+}
+
+let benz = new Car('G-Wagon');
+console.log(benz.carId);
+benz.start();
