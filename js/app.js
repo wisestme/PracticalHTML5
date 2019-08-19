787aad2ea64be08b1cc11581fbea4e8e61435077
wisestMe <wyhzest@gmail.com>
2019-08-19 10:00:35 +0100

    adds a method to constructor function

--- js/app.js
+++ js/app.js
@@ -4,8 +4,13 @@ cartAdd.onclick = function () {
 	alert('item added to your cart');
 };
 
-function Car() {
-
+function Car(model) {
+	this.model = model;
+	this.startEngine = function () {
+		console.log('Starting engine sound');
+	}
 }
 
-let benz = new Car();
+let benz = new Car("G-Wagon");
+console.log(benz.model);
+benz.startEngine();
