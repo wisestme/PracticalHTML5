058a199b5832a46ad205fcba523690e9a2e26adf
wisestMe <wyhzest@gmail.com>
2019-08-21 08:04:48 +0100

    deletes test code

--- js/app.js
+++ js/app.js
@@ -4,15 +4,6 @@ cartAdd.onclick = function () {
 	alert('item added to your cart');
 };
 
-function Car(model) {
-	this.model = model;
-	
-}
 
-Car.prototype.startEngine = function () {
-		console.log(`Starting ${this.model} engine sound`);
-	}
 
-let benz = new Car("G-Wagon");
-console.log(benz.model);
-benz.startEngine();
+
