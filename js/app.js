d73bde3ab438731170440da1f450d1e2b0ba0f1c
wisestMe <wyhzest@gmail.com>
2019-08-28 08:12:53 +0100

    deletes test codes

--- js/app.js
+++ js/app.js
@@ -4,19 +4,3 @@ cartAdd.onclick = function () {
 	alert('item added to your cart');
 };
 
-class Gadget {
-	constructor(type) {
-		this.type = type;
-	}
-	model(number) {
-		return `Phone Model: Galaxy ${this.type} ${number}`
-	}
-}
-
-class Phone extends Gadget {
-
-}
-
-let samsung = new Phone('Note10');
-
-console.log(samsung.type);
