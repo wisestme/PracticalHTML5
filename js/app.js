0514b9c0d578736cc52b999c4edf65a8ecd6e385
wisestMe <wyhzest@gmail.com>
2019-08-29 20:46:46 +0100

    implements time function

--- js/app.js
+++ js/app.js
@@ -4,3 +4,9 @@ cartAdd.onclick = function () {
 	alert('item added to your cart');
 };
 
+let x = 0;
+let secsCounter = setInterval(function () {
+	
+	x += 1;
+	console.log(`${x} seconds elapsed`);
+}, 1000)
