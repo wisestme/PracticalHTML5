6f11a63e263d5b154e1f5a46b5d0fd75fa5208b3
wisestMe <wyhzest@gmail.com>
2019-09-01 20:09:35 +0100

    deletes test code

--- js/app.js
+++ js/app.js
@@ -4,15 +4,3 @@ cartAdd.onclick = function () {
 	alert('item added to your cart');
 };
 
-let x = 0;
-let secsCounter = setInterval(function () {
-	
-	x += 1;
-	console.log(`${x} seconds elapsed`);
-
-	if (x === 10) {
-		clearInterval(secsCounter);
-	}
-
-}, 1000)
-
