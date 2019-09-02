d238e3c932ccccd4d7c73e4262184e4aca31f50c
wisestMe <wyhzest@gmail.com>
2019-09-02 07:48:40 +0100

    deletes test codes

--- js/app.js
+++ js/app.js
@@ -4,14 +4,4 @@ cartAdd.onclick = function () {
 	alert('item added to your cart');
 };
 
-try {
-	throw new Error('my custom error');
-}
 
-catch(error) {
-	console.log('error:',error);
-}
-
-finally {
-	console.log('continuing... this block must run');
-}
