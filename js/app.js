844c9a2d519c986fb29d12ddc12bef7ac4f9e94a
wisestMe <wyhzest@gmail.com>
2019-09-05 06:49:19 +0100

    sets validation error message

--- js/app.js
+++ js/app.js
@@ -5,19 +5,27 @@ cartAdd.onclick = function () {
 };*/
 
 let form = document.getElementById('main-form');
-let firstName = form.elements['first-name'];
-let lastName = form.elements['last-name'];
 
-let userError = document.getElementByClassName('user-error');
+
+
+
+form.addEventListener('submit', event => {
+	event.preventDefault();
+
+	let bows = form.elements['Bows'];
+	let wrapper = form.elements['Wrapper color'];
+	console.log(bows.value, wrapper.value);
+
+	let firstName = form.elements['first-name'];
+	let lastName = form.elements['last-name'];
+	let userError = document.getElementById('user-error');
+	console.log(userError.textContent);
 userError.textContent = 'Invalid entry';
 userError.style.color = 'red';
 firstName.style.borderColor = 'red';
 firstName.focus();
 
-form.addEventListener('submit', event => {
-	let bows = form.elements['Bows'];
-	let wrapper = form.elements['Wrapper color'];
-	console.log(bows.value, wrapper.value);
-	event.preventDefault();
+
+	
 });
 
