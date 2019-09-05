bf6e6366eff4b2b3812295436a4d7ebccbe5f90b
wisestMe <wyhzest@gmail.com>
2019-09-05 05:48:33 +0100

    gets access to form elements

--- js/app.js
+++ js/app.js
@@ -1,12 +1,15 @@
-var cartAdd  = document.getElementById('add-to-cart');
+/*var cartAdd  = document.getElementById('add-to-cart');
 
 cartAdd.onclick = function () {
 	alert('item added to your cart');
-};
+};*/
 
 let form = document.getElementById('main-form');
 
 form.addEventListener('submit', event => {
+	let bows = form.elements['Bows'];
+	let wrapper = form.elements['Wrapper color'];
+	console.log(bows.value, wrapper.value);
 	event.preventDefault();
 });
 
